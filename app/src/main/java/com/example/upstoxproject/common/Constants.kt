a197be3d865b3184ccf5c65b2c2f1b8a5e23455b
Ashish Lattukids <Ashish Gupta>
2023-03-25 17:32:19 +0530

    added constants file

---
+++ app/src/main/java/com/example/upstoxproject/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.plcoding.cryptocurrencyappyt.common
+
+object Constants {
+    const val BASE_URL = "https://run.mocky.io/"
+}
---
+++ app/src/main/java/com/example/upstoxproject/common/Resource.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cryptocurrencyappyt.common
+
+sealed class Resource<out T>(val data:T? = null, val message:String? = null) {
+
+    class Success<T>(data:T):Resource<T>(data)
+    class Error<T>(data:T? = null, message: String):Resource<T>(data,message)
+    class Loading<T>(data:T? = null):Resource<T>(data)
+
+}
